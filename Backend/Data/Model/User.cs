695be26b12026f3af2f096bc9d8d529056163a5b
JP Dillingham <jpdillingham@gmail.com>
2018-04-12 22:47:32 -0500

    add role and user

---
+++ Backend/Data/Model/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace QCVOC.Backend.Data.Model
+{
+    public class User
+    {
+        #region Public Properties
+
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string PasswordSHA256 { get; set; }
+        public Role Role { get; set; }
+
+        #endregion Public Properties
+    }
+}
