ccdefd1cee63ae00182323e9138c8c9b5297710e
JP Dillingham <jpdillingham@gmail.com>
2018-04-12 21:15:34 -0500

    stub sessions controller

---
+++ Backend/Controllers/SessionsController.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+using QCVOC.DTO;
+
+namespace QCVOC.Backend.Controllers
+{
+    [Route("api/[controller]")]
+    public class SessionsController : Controller
+    {
+        // GET api/values
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("test");
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public IActionResult Post([FromBody]SessionInfo sessionInfo)
+        {
+            return Ok(sessionInfo);
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
