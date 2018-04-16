fc753e31158276f02548ceb4ab85d6cbc9236fd3
JP Dillingham <jpdillingham@gmail.com>
2018-04-15 21:58:26 -0500

    add IJwtFactory

---
+++ QCVOC.Server/Security/IJwtFactory.cs
@@ -0,0 +1,13 @@
+﻿using QCVOC.Server.Data.Model;
+
+namespace QCVOC.Server.Security
+{
+    internal interface IJwtFactory
+    {
+        #region Public Methods
+
+        string GetJwt(User user);
+
+        #endregion Public Methods
+    }
+}
