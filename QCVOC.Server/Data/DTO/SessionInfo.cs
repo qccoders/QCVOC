d65193a2f783d34a2e3c87ac892ea74ab11f8fae
JP Dillingham <jpdillingham@gmail.com>
2018-04-15 22:09:57 -0500

    change Username to Name

--- QCVOC.Server/Data/DTO/SessionInfo.cs
+++ QCVOC.Server/Data/DTO/SessionInfo.cs
@@ -1,7 +1,7 @@
 namespace QCVOC.Data.DTO {
     public class SessionInfo
     {
-        public string Username { get; set; }
+        public string Name { get; set; }
         public string Password { get; set; }
     }
 }
