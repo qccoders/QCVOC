70a537998462b335497fc5324cb28a8e34f9faaa
JP Dillingham <jpdillingham@gmail.com>
2018-04-15 20:32:10 -0500

    update project structure

--- Backend/Data/DTO/SessionInfo.cs
+++
@@ -1,8 +0,0 @@
-namespace QCVOC.Data.DTO {
-    public class SessionInfo
-    {
-        public string Username { get; set; }
-        public string Password { get; set; }
-    }
-}
-
--- Backend/Data/Model/Role.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace QCVOC.Backend.Data.Model
-{
-    public enum Role
-    {
-        User = 0,
-        Supervisor = 1,
-        Administrator = 2,
-    }
-}
