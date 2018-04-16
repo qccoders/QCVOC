1847cd0c53331d47007ba37de8e7538afe111bb4
JP Dillingham <jpdillingham@gmail.com>
2018-04-15 22:14:05 -0500

    add password hash

--- QCVOC.Server/Data/Repository/UserRepository.cs
+++ QCVOC.Server/Data/Repository/UserRepository.cs
@@ -13,7 +13,7 @@ namespace QCVOC.Server.Data.Repository
         {
             Id = Guid.NewGuid(),
             Name = "test",
-            PasswordHash = "",
+            PasswordHash = "EE26B0DD4AF7E749AA1A8EE3C10AE9923F618980772E473F8819A5D4940E0DB27AC185F8A0E1D5F84F88BC887FD67B143732C304CC5FA9AD8E6F57F50028A8FF", // test
             Role = Role.Administrator
         };
 
