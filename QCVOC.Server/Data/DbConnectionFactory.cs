8d5028e2b90297a42fb576f21a9c1c69f99a6071
JP Dillingham <jpdillingham@gmail.com>
2018-04-19 07:38:38 -0500

    add DbConnectionFactory

---
+++ QCVOC.Server/Data/DbConnectionFactory.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace QCVOC.Server.Data
+{
+    public class DbConnectionFactory
+    {
+        #region Public Methods
+
+        public IDbConnection GetDbConnection<T>(string connectionString)
+        {
+            if (typeof(T) == typeof(SqlConnection))
+            {
+                return new SqlConnection(connectionString);
+            }
+            else
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        #endregion Public Methods
+    }
+}
