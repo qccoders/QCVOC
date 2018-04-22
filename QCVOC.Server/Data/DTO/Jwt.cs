43bbafa5957a57b63e18305dbd359cd020496e52
JP Dillingham <jpdillingham@gmail.com>
2018-04-22 17:17:40 -0500

    add Jwt DTO

---
+++ QCVOC.Server/Data/DTO/Jwt.cs
@@ -0,0 +1,17 @@
+﻿using QCVOC.Server.Data.Model;
+using System;
+
+namespace QCVOC.Server.Data.DTO
+{
+    public class Jwt
+    {
+        #region Public Properties
+
+        public DateTime Expires { get; set; }
+        public string Name { get; set; }
+        public Role Role { get; set; }
+        public string Token { get; set; }
+
+        #endregion Public Properties
+    }
+}
