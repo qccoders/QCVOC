7d20d7baa828ddccf1dc1a2033166b88e25fdc12
JP Dillingham <jpdillingham@gmail.com>
2018-05-05 22:55:58 -0500

    add file header

--- QCVOC.Server/Data/Repository/IRepository{T}.cs
+++ QCVOC.Server/Data/Repository/IRepository{T}.cs
@@ -1,4 +1,9 @@
-﻿namespace QCVOC.Server.Data.Repository
+﻿// <copyright file="IRepository{T}.cs" company="JP Dillingham, Nick Acosta, et. al.">
+//     Copyright (c) JP Dillingham, Nick Acosta, et. al.. All rights reserved. Licensed under the GPLv3 license. See LICENSE file
+//     in the project root for full license information.
+// </copyright>
+
+namespace QCVOC.Server.Data.Repository
 {
     using System;
     using System.Collections.Generic;
