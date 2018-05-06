fc267d1cb32aa610009c1193e20ada380a92b3a5
JP Dillingham <jpdillingham@gmail.com>
2018-05-05 23:28:37 -0500

    clean up Account

--- QCVOC.Server/Data/Model/Account.cs
+++ QCVOC.Server/Data/Model/Account.cs
@@ -1,7 +1,12 @@
-﻿using System;
+﻿// <copyright file="Account.cs" company="JP Dillingham, Nick Acosta, et. al.">
+//     Copyright (c) JP Dillingham, Nick Acosta, et. al.. All rights reserved. Licensed under the GPLv3 license. See LICENSE file
+//     in the project root for full license information.
+// </copyright>
 
 namespace QCVOC.Server.Data.Model
 {
+    using System;
+
     public class Account
     {
         #region Public Properties
