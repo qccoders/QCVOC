a2efda466b2e04c3c52e0f01d78dd78c0134e546
JP Dillingham <jpdillingham@gmail.com>
2018-05-05 23:28:53 -0500

    add AccountResponse dto

---
+++ QCVOC.Server/Data/DTO/AccountResponse.cs
@@ -0,0 +1,21 @@
+﻿// <copyright file="AccountResponse.cs" company="JP Dillingham, Nick Acosta, et. al.">
+//     Copyright (c) JP Dillingham, Nick Acosta, et. al.. All rights reserved. Licensed under the GPLv3 license. See LICENSE file
+//     in the project root for full license information.
+// </copyright>
+
+namespace QCVOC.Server.Data.DTO
+{
+    using System;
+    using QCVOC.Server.Data.Model;
+
+    public class AccountResponse
+    {
+        #region Public Properties
+
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public Role Role { get; set; }
+
+        #endregion Public Properties
+    }
+}
