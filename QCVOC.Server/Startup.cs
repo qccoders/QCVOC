ff34722fc02918df08a99ab29b6db3d3d45b905a
JP Dillingham <jpdillingham@gmail.com>
2018-05-06 22:32:56 -0500

    copy/paste a bunch of crap from: https://github.com/Microsoft/aspnet-api-versioning/blob/master/samples/aspnetcore/SwaggerSample/Startup.cs

--- QCVOC.Server/Startup.cs
+++ QCVOC.Server/Startup.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ApiExplorer;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.PlatformAbstractions;
@@ -9,6 +10,7 @@ using Microsoft.IdentityModel.Tokens;
 using NLog;
 using Npgsql;
 using QCVOC.Server.Data.ConnectionFactory;
+using QCVOC.Server.Data.Model;
 using QCVOC.Server.Data.Repository;
 using QCVOC.Server.Middleware;
 using QCVOC.Server.Security;
@@ -47,7 +49,7 @@ namespace QCVOC.Server
 
         #region Public Methods
 
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, IApiVersionDescriptionProvider provider)
         {
             if (env.IsDevelopment())
             {
@@ -62,17 +64,22 @@ namespace QCVOC.Server
             app.UseSwagger();
             app.UseSwaggerUI(c =>
             {
-                c.SwaggerEndpoint("/swagger/v1/swagger.json", "QCVOC API V1");
-                c.SwaggerEndpoint("/swagger/v2/swagger.json", "QCVOC API V2");
+                foreach (var description in provider.ApiVersionDescriptions)
+                {
+                    c.SwaggerEndpoint($"/swagger/{description.GroupName}/swagger.json", description.GroupName.ToUpperInvariant());
+                }
             });
         }
 
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddTransient<IAccountRepository, AccountRepository>();
             services.AddTransient<IJwtFactory, JwtFactory>();
+
             services.AddSingleton<IDbConnectionFactory, NpgsqlDbConnectionFactory>(serviceProvider =>
                 new NpgsqlDbConnectionFactory("User ID=QCVOC;Password=QCVOC;Host=SQL;Port=5432;Database=QCVOC;Pooling = true;"));
+
+            services.AddTransient<IRepository<Account>, Repository<Account>>(serviceProvider =>
+                new Repository<Account>(serviceProvider.GetService<IDbConnectionFactory>()));
 
             services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
                 .AddJwtBearer(options =>
@@ -88,6 +95,18 @@ namespace QCVOC.Server
                     };
                 });
 
+            // add the versioned api explorer, which also adds IApiVersionDescriptionProvider service
+            // note: the specified format code will format the version as "'v'major[.minor][-status]"
+            services.AddMvcCore().AddVersionedApiExplorer(
+                options =>
+                {
+                    options.GroupNameFormat = "'v'VVV";
+
+                    // note: this option is only necessary when versioning by url segment. the SubstitutionFormat can also be used
+                    // to control the format of the API version in route templates
+                    options.SubstituteApiVersionInUrl = true;
+                });
+
             services.AddMvc();
 
             services.AddApiVersioning(o =>
@@ -99,8 +118,16 @@ namespace QCVOC.Server
 
             services.AddSwaggerGen(c =>
             {
-                c.SwaggerDoc("v1", new Info { Title = "QCVOC API V1", Version = "v1" });
-                c.SwaggerDoc("v2", new Info { Title = "QCVOC API V2", Version = "v2" });
+                // resolve the IApiVersionDescriptionProvider service
+                // note: that we have to build a temporary service provider here because one has not been created yet
+                var provider = services.BuildServiceProvider().GetRequiredService<IApiVersionDescriptionProvider>();
+
+                // add a swagger document for each discovered API version
+                // note: you might choose to skip or document deprecated API versions differently
+                foreach (var description in provider.ApiVersionDescriptions)
+                {
+                    c.SwaggerDoc(description.GroupName, CreateInfoForApiVersion(description));
+                }
 
                 c.DocInclusionPredicate((docName, apiDesc) =>
                 {
@@ -130,5 +157,29 @@ namespace QCVOC.Server
         }
 
         #endregion Public Methods
+
+        #region Private Methods
+
+        private static Info CreateInfoForApiVersion(ApiVersionDescription description)
+        {
+            var info = new Info()
+            {
+                Title = $"Sample API {description.ApiVersion}",
+                Version = description.ApiVersion.ToString(),
+                Description = "A sample application with Swagger, Swashbuckle, and API versioning.",
+                Contact = new Contact() { Name = "Bill Mei", Email = "bill.mei@somewhere.com" },
+                TermsOfService = "Shareware",
+                License = new License() { Name = "MIT", Url = "https://opensource.org/licenses/MIT" }
+            };
+
+            if (description.IsDeprecated)
+            {
+                info.Description += " This API version has been deprecated.";
+            }
+
+            return info;
+        }
+
+        #endregion Private Methods
     }
 }
