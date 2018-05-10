e4965c2e7b678b3afb8f892b459aa27ea8fd2839
JP Dillingham <jpdillingham@gmail.com>
2018-05-09 22:23:08 -0500

    add Constants and Settings to avoid magic strings

---
+++ QCVOC.Server/Settings.cs
@@ -0,0 +1,15 @@
+﻿namespace QCVOC.Server
+{
+    public static class Settings
+    {
+        #region Public Fields
+
+        public static readonly string JwtAccessTokenExpiry = "JwtAccessTokenExpiry";
+        public static readonly string JwtAudience = "JwtAudience";
+        public static readonly string JwtIssuer = "JwtIssuer";
+        public static readonly string JwtKey = "JwtKey";
+        public static readonly string JwtRefreshTokenExpiry = "JwtRefreshTokenExpiry";
+
+        #endregion Public Fields
+    }
+}
