1d3786550a1bc3884350f77c122a786ffad71a8e
JP Dillingham <jpdillingham@gmail.com>
2018-05-12 16:25:59 -0500

    add Jwt and TokenValidationParametersFactory back

---
+++ QCVOC.Server/Security/TokenValidationParametersFactory.cs
@@ -0,0 +1,26 @@
+﻿namespace QCVOC.Server.Security
+{
+    using System.Text;
+    using Microsoft.IdentityModel.Tokens;
+    using Utility = Utility;
+
+    public class TokenValidationParametersFactory
+    {
+        #region Public Methods
+
+        public TokenValidationParameters GetParameters()
+        {
+            return new TokenValidationParameters
+            {
+                ValidIssuer = Utility.GetSetting<string>(Settings.JwtIssuer, Constants.JwtIssuerDefault),
+                ValidateIssuer = true,
+                ValidAudience = Utility.GetSetting<string>(Settings.JwtAudience, Constants.JwtAudienceDefault),
+                ValidateAudience = true,
+                IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Utility.GetSetting<string>(Settings.JwtKey, Constants.JwtKeyDefault))),
+                ValidateIssuerSigningKey = true,
+            };
+        }
+
+        #endregion Public Methods
+    }
+}
