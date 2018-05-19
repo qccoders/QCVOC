00925fa946bc6f1c76337b72ac3dd385c6ac6d23
JP Dillingham <jpdillingham@gmail.com>
2018-05-19 13:11:29 -0500

    simplify I/JwtFactory

--- QCVOC.Server/Security/IJwtFactory.cs
+++ QCVOC.Server/Security/IJwtFactory.cs
@@ -1,25 +1,15 @@
 ﻿namespace QCVOC.Server.Security
 {
     using System;
-    using System.IdentityModel.Tokens.Jwt;
-    using Microsoft.IdentityModel.Tokens;
     using QCVOC.Server.Data.Model.Security;
 
     public interface IJwtFactory
     {
         #region Public Methods
 
-        JwtSecurityToken GetAccessToken(Account account, Guid refreshTokenId);
-
         Jwt GetJwt(Account account);
 
         Jwt GetJwt(Account account, Guid refreshTokenId);
-
-        JwtSecurityToken GetRefreshToken(Guid refreshTokenId);
-
-        TokenValidationParameters GetTokenValidationParameters();
-
-        bool TryParseAndValidateToken(string token, out JwtSecurityToken jwtSecurityToken);
 
         #endregion Public Methods
     }
