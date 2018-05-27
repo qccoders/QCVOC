bcf4280bf9dd748c46bf39c60cad2afb95b5a6fc
JP Dillingham <jpdillingham@gmail.com>
2018-05-26 22:22:22 -0500

    refactor Jwt to TokenResponse

--- QCVOC.Server/Data/DTO/TokenResponse.cs
+++ QCVOC.Server/Data/DTO/TokenResponse.cs
@@ -1,14 +1,23 @@
-﻿namespace QCVOC.Server.Security
+﻿namespace QCVOC.Server.Data.DTO
 {
     using System;
     using System.IdentityModel.Tokens.Jwt;
-    using System.Linq;
-    using System.Security.Claims;
+    using Microsoft.AspNetCore.Authentication.JwtBearer;
     using Newtonsoft.Json;
     using QCVOC.Server.Data.Model.Security;
 
-    public class Jwt
+    public class TokenResponse
     {
+        #region Public Constructors
+
+        public TokenResponse(JwtSecurityToken accessJwtSecurityToken, JwtSecurityToken refreshJwtSecurityToken)
+        {
+            AccessJwtSecurityToken = accessJwtSecurityToken;
+            RefreshJwtSecurityToken = refreshJwtSecurityToken;
+        }
+
+        #endregion Public Constructors
+
         #region Public Properties
 
         [JsonIgnore]
@@ -29,6 +38,8 @@
 
         [JsonIgnore]
         public Guid RefreshTokenId => Guid.Parse(RefreshJwtSecurityToken.Id);
+
+        public string TokenType => JwtBearerDefaults.AuthenticationScheme;
 
         #endregion Public Properties
     }
