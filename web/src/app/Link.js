e9bc423b85b4b21f65fd68ae6b5437f04d177071
JP Dillingham <jpdillingham@gmail.com>
2018-07-29 18:30:26 -0500

    make Link a functional stateless component

--- web/src/app/Link.js
+++ web/src/app/Link.js
@@ -1,31 +1,26 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Link as RouterLink } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
-import { withStyles } from '@material-ui/core/styles';
-import { ListItem, ListItemIcon, ListItemText } from '../../node_modules/@material-ui/core';
+import { ListItem, ListItemIcon, ListItemText } from '@material-ui/core';
 
-const styles = {
-
-};
-
-class Link extends Component {
-    render() {
-        let classes = this.props.classes;
-
-        return (
-            <RouterLink to={this.props.to}>
-                <ListItem button>
-                    <ListItemIcon>{this.props.icon}</ListItemIcon>
-                    <ListItemText primary={this.props.children}/>
-                </ListItem>
-            </RouterLink>
-        );
-    }
+const Link = (props) => {
+    return (
+        <ListItem 
+            button 
+            component={RouterLink} 
+            to={props.to}
+        >            
+            <ListItemIcon>{props.icon}</ListItemIcon>
+            <ListItemText primary={props.children}/>
+        </ListItem>
+    );
 }
 
 Link.propTypes = {
-  classes: PropTypes.object.isRequired,
+  icon: PropTypes.object.isRequired,
+  children: PropTypes.string,
+  to: PropTypes.string,
 };
 
-export default withStyles(styles)(Link); 
+export default Link; 
