b7bf3cc9d9d46d0d08d46149612567054deb7962
JP Dillingham <jpdillingham@gmail.com>
2018-07-29 14:00:44 -0500

    remove boilerplate

---
+++ web/src/app/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <h1>QCVOC</h1>
+    );
+  }
+}
+
+export default App;
