303e002e412ea3135053e2decb8e5a818a5b5c43
JP Dillingham <jpdillingham@gmail.com>
2018-07-30 22:48:22 -0500

    test

--- api/QCVOC.Api/Data/Model/Event.cs
+++ api/QCVOC.Api/Data/Model/Event.cs
@@ -2,6 +2,7 @@
 //     Copyright (c) JP Dillingham, Nick Acosta, et. al.. All rights reserved. Licensed under the GPLv3 license. See LICENSE file
 //     in the project root for full license information.
 // </copyright>
+// a
 
 namespace QCVOC.Api.Data.Model
 {
