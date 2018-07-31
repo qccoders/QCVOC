15913effe4cc669220b99b1a0c5e0d82dbbe62a7
JP Dillingham <jpdillingham@gmail.com>
2018-07-30 22:57:05 -0500

    revert test

--- api/QCVOC.Api/Data/Model/Event.cs
+++ api/QCVOC.Api/Data/Model/Event.cs
@@ -2,7 +2,6 @@
 //     Copyright (c) JP Dillingham, Nick Acosta, et. al.. All rights reserved. Licensed under the GPLv3 license. See LICENSE file
 //     in the project root for full license information.
 // </copyright>
-// a
 
 namespace QCVOC.Api.Data.Model
 {
