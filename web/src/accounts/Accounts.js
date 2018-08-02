63ae87d22266c580638864618ba3f9c7ae8f9a0e
JP Dillingham <jpdillingham@gmail.com>
2018-08-01 22:38:48 -0500

    stub account fetch

--- web/src/accounts/Accounts.js
+++ web/src/accounts/Accounts.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
+import { API_ROOT } from '../constants';
+
 import { withStyles } from '@material-ui/core/styles';
 
 const styles = {
@@ -10,8 +12,20 @@ const styles = {
 };
 
 class Accounts extends Component {
+    state = { accounts: [] };
+
+    componentWillMount = () => {
+        fetch(API_ROOT + '/v1/accounts')
+        .then(response => response.json())
+        .then(data => {
+            this.setState({ accounts: data });
+        });
+    }
+
     render() {
         let classes = this.props.classes;
+
+        console.log(this.state.accounts);
 
         return (
             <div className={classes.root}>
