d4a171edabbdda41b3f415eb186b3cfba7f7726c
JP Dillingham <jpdillingham@gmail.com>
2018-08-03 23:07:10 -0500

    add LogoutButton component

---
+++ web/src/app/LogoutButton.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
+import ExitToAppIcon from '@material-ui/icons/ExitToApp';
+
+const styles = {
+    container: {
+        marginLeft: 'auto',
+        whiteSpace: 'pre',
+    },
+    caption: {
+        marginRight: 10,
+    },
+}
+
+const LogoutButton = (props) => {
+    return (
+        <div style={styles.container}>
+            <IconButton
+                color="inherit"
+            >
+                <Typography color="inherit" style={styles.caption}>Log Out</Typography>
+                <ExitToAppIcon/>
+            </IconButton>
+        </div>
+    );
+}
+
+export default LogoutButton; 
