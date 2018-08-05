c6d86032a00337dda4599a69d4a185be45c6d97b
JP Dillingham <jpdillingham@gmail.com>
2018-08-05 13:36:30 -0500

    add validation and status indicators

--- web/src/app/LoginForm.js
+++ web/src/app/LoginForm.js
@@ -5,7 +5,7 @@ import axios from 'axios';
 import { API_ROOT } from '../constants';
 import { withStyles } from '@material-ui/core/styles';
 
-import { Card, CardContent, CardActions } from '@material-ui/core';
+import { Card, CardContent, CardActions, CircularProgress } from '@material-ui/core';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
@@ -41,26 +41,73 @@ const styles = {
         width: 339,
         height: 158,
     },
+    spinner: {
+        position: 'fixed',
+    },
 };
 
+const initialState = {
+    name: '',
+    password: '',
+    rememberMe: true,
+    api: {
+        isExecuting: false,
+        isErrored: false,
+    },
+    validation: {
+        name: undefined,
+        password: undefined,
+    }
+}
+
 class LoginForm extends Component {
-    state = { name: '', password: '', rememberMe: true }
+    state = initialState;
 
     handleChange = (field, event) => {
-        this.setState({ [field]: event.target.value });
+        this.setState({ 
+            ...this.state, 
+            [field]: event.target.value,
+            validation: {
+                ...this.state.validation,
+                [field]: undefined,
+            },
+        });
     }
 
     handleLoginClick = () => {
-        axios.post(API_ROOT + '/v1/tokens', this.state)
-        .then(
-            response => this.props.onLogin(response.data, this.state.rememberMe), 
-            error => console.log(error.response)
-        );
+        this.setState({ api: { isExecuting: true }}, () => {
+            axios.post(API_ROOT + '/v1/tokens', this.state)
+            .then(
+                response => {
+                    this.props.onLogin(response.data, this.state.rememberMe);
+                }, 
+                error => {
+                    console.log(error.response)
+
+                    this.setState({ api: { isExecuting: false, isErrored: true }}, () => {
+                        if (error.response.data) {
+                            let validation = { ... this.state.validation };
+
+                            if (error.response.data.Name && error.response.data.Name.length > 0) {
+                                validation.name = error.response.data.Name[0];
+                            }
+                            if (error.response.data.Password && error.response.data.Password.length > 0) {
+                                validation.password = error.response.data.Password[0];
+                            }
+
+                            this.setState({ ...this.state, validation: validation });
+                        }
+                    })
+                }
+            );
+        })
     }
 
     render() {
         let classes = this.props.classes;
+        let isExecuting = this.state.api.isExecuting;
 
+        console.log(this.state);
         return (
             <div className={classes.root}>
                 <Card className={classes.card}>
@@ -71,7 +118,10 @@ class LoginForm extends Component {
                             label="Name"
                             className={classes.textField}
                             value={this.state.name}
+                            error={this.state.validation.name !== undefined}
+                            helperText={this.state.validation.name}
                             margin="normal"
+                            disabled={isExecuting}
                             onChange={(event) => this.handleChange('name', event)}
                         />
                         <TextField
@@ -81,11 +131,15 @@ class LoginForm extends Component {
                             type="password"
                             autoComplete="current-password"
                             margin="normal"
+                            disabled={isExecuting}
+                            error={this.state.validation.password !== undefined}
+                            helperText={this.state.validation.password}
                             onChange={(event) => this.handleChange('password', event)}
                         />
                         <FormControlLabel
                             className={classes.checkbox}
                             label="Remember Me"
+                            disabled={isExecuting}
                             control={
                                 <Checkbox
                                     checked={this.state.rememberMe}
@@ -101,7 +155,9 @@ class LoginForm extends Component {
                             color='secondary'
                             className={classes.button}
                             onClick={this.handleLoginClick}
+                            disabled={isExecuting}
                         >
+                            {isExecuting ? <CircularProgress size={20} style={styles.spinner}/> : ''}
                             Log In
                         </Button>
                     </CardActions>
