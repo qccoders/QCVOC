5bffe199dfafd16f451c82626eb3752f26af14b9
JP Dillingham <jpdillingham@gmail.com>
2018-08-05 21:28:55 -0500

    add missing header

--- api/QCVOC.Api/Data/Repository/RefreshTokenRepository.cs
+++ api/QCVOC.Api/Data/Repository/RefreshTokenRepository.cs
@@ -1,3 +1,8 @@
+// <copyright file="RefreshTokenRepository.cs" company="JP Dillingham, Nick Acosta, et. al.">
+//     Copyright (c) JP Dillingham, Nick Acosta, et. al.. All rights reserved. Licensed under the GPLv3 license. See LICENSE file
+//     in the project root for full license information.
+// </copyright>
+
 namespace QCVOC.Api.Data.Repository
 {
     using System;
