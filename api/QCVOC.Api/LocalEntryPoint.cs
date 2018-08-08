c21af302bc7198f062b449c872d68d6c689bc594
JP Dillingham <jpdillingham@gmail.com>
2018-08-08 18:05:59 -0500

    rename Program to LocalEntryPoint

--- api/QCVOC.Api/LocalEntryPoint.cs
+++ api/QCVOC.Api/LocalEntryPoint.cs
@@ -1,4 +1,4 @@
-﻿// <copyright file="Program.cs" company="JP Dillingham, Nick Acosta, et. al.">
+﻿// <copyright file="LocalEntryPoint.cs" company="JP Dillingham, Nick Acosta, et. al.">
 //     Copyright (c) JP Dillingham, Nick Acosta, et. al.. All rights reserved. Licensed under the GPLv3 license. See LICENSE file
 //     in the project root for full license information.
 // </copyright>
@@ -9,9 +9,9 @@ namespace QCVOC.Api
     using Microsoft.AspNetCore.Hosting;
 
     /// <summary>
-    ///     The main class for the application.
+    ///     The local entry point for the application.
     /// </summary>
-    public class Program
+    public class LocalEntryPoint
     {
         /// <summary>
         ///     Builds the application <see cref="IWebHost"/>.
