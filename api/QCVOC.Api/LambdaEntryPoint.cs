4e48cf7a0adc0ba499d747418a7603d455c210e0
JP Dillingham <jpdillingham@gmail.com>
2018-08-08 19:01:45 -0500

    add LambdaEntryPoint

---
+++ api/QCVOC.Api/LambdaEntryPoint.cs
@@ -0,0 +1,15 @@
+﻿namespace QCVOC.Api
+{
+    using Amazon.Lambda.AspNetCoreServer;
+    using Microsoft.AspNetCore.Hosting;
+
+    public class LambdaEntryPoint : APIGatewayProxyFunction
+    {
+        protected override void Init(IWebHostBuilder builder)
+        {
+            builder
+                .UseStartup<Startup>()
+                .UseApiGateway();
+        }
+    }
+}
