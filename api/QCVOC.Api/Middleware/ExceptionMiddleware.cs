9d6d9ff95dd4aba80c545290844b0d8c7ff82643
JP Dillingham <jpdillingham@gmail.com>
2018-08-11 14:42:24 -0500

    add ExceptionMiddleware

---
+++ api/QCVOC.Api/Middleware/ExceptionMiddleware.cs
@@ -0,0 +1,44 @@
+﻿namespace QCVOC.Api.Middleware
+{
+    using Microsoft.AspNetCore.Http;
+    using Newtonsoft.Json;
+    using NLog;
+    using System;
+    using System.Threading.Tasks;
+
+    public class ExceptionMiddleware
+    {
+        private readonly Logger logger = LogManager.GetCurrentClassLogger();
+        private readonly RequestDelegate next;
+
+        public ExceptionMiddleware(RequestDelegate next)
+        {
+            this.next = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            try
+            {
+                await next(context);
+            }
+            catch (Exception ex)
+            {
+                logger.Error(ex);
+                await HandleExceptionAsync(context, ex);
+            }
+        }
+
+        private static Task HandleExceptionAsync(HttpContext context, Exception exception)
+        {
+            if (context.Response.HasStarted)
+            {
+                return Task.CompletedTask;
+            }
+
+            context.Response.StatusCode = 500;
+            context.Response.ContentType = "application/json";
+            return context.Response.WriteAsync(JsonConvert.SerializeObject(exception));
+        }
+    }
+}
