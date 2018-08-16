a55ffb3aaec15948f1ee295f40c8b3855b617926
JP Dillingham <jpdillingham@gmail.com>
2018-08-15 22:06:02 -0500

    add getGuid to util

--- web/src/util.js
+++ web/src/util.js
@@ -15,3 +15,13 @@ export const sortByProp = (prop, predicate = 'asc') => {
         }
     }
 }
+
+export const getGuid = () => {
+    function s4() {
+        return Math.floor((1 + Math.random()) * 0x10000)
+        .toString(16)
+        .substring(1);
+    }
+    return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
+        s4() + '-' + s4() + s4() + s4();
+}
