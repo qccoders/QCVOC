e5555d58178e589384ae551974f6f589fd75e91c
JP Dillingham <jpdillingham@gmail.com>
2018-08-20 08:31:00 -0500

    add RefreshTokenQueryParameters

---
+++ api/QCVOC.Api/Security/RefreshTokenQueryParameters.cs
@@ -0,0 +1,10 @@
+﻿namespace QCVOC.Api.Security
+{
+    using System;
+    using QCVOC.Api.Common;
+
+    public class RefreshTokenQueryParameters : QueryParameters
+    {
+        public Guid? AccountId { get; set; }
+    }
+}
