166112a3092659aa6ad4bfab7bfa717f31e14e3b
JP Dillingham <jpdillingham@gmail.com>
2018-08-19 20:52:14 -0500

    add MiddlewareExtensions

---
+++ api/QCVOC.Api/Middleware/MiddlewareExtensions.cs
@@ -0,0 +1,17 @@
+﻿namespace QCVOC.Api.Middleware
+{
+    using Microsoft.AspNetCore.Builder;
+
+    public static class MiddlewareExtensions
+    {
+        public static IApplicationBuilder UseExceptionMiddleware(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<ExceptionMiddleware>();
+        }
+
+        public static IApplicationBuilder UseLogger(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<LoggingMiddleware>();
+        }
+    }
+}
