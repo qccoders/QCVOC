13ef9b369388d7d748c95e731b05b9ec5dbbff6f
JP Dillingham <jpdillingham@gmail.com>
2018-08-21 07:37:21 -0500

    document RefreshTokenQueryParameters

--- api/QCVOC.Api/Security/RefreshTokenQueryParameters.cs
+++ api/QCVOC.Api/Security/RefreshTokenQueryParameters.cs
@@ -1,10 +1,23 @@
-﻿namespace QCVOC.Api.Security
+﻿// <copyright file="RefreshTokenQueryParameters.cs" company="JP Dillingham, Nick Acosta, et. al.">
+//     Copyright (c) JP Dillingham, Nick Acosta, et. al.. All rights reserved. Licensed under the GPLv3 license. See LICENSE file
+//     in the project root for full license information.
+// </copyright>
+
+namespace QCVOC.Api.Security
 {
     using System;
+    using System.Diagnostics.CodeAnalysis;
     using QCVOC.Api.Common;
 
+    /// <summary>
+    ///     Refresh token pagination and filtering options.
+    /// </summary>
+    [SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1623:PropertySummaryDocumentationMustMatchAccessors", Justification = "Omitted for Swashbuckle compatibility.")]
     public class RefreshTokenQueryParameters : QueryParameters
     {
+        /// <summary>
+        ///     The Account id by which to filter results.
+        /// </summary>
         public Guid? AccountId { get; set; }
     }
 }
