ea251ded5f55bf8eb6ff9937ac733277012e9bad
JP Dillingham <jpdillingham@gmail.com>
2018-08-21 07:37:09 -0500

    document AccountQueryParameters

--- api/QCVOC.Api/Security/AccountQueryParameters.cs
+++ api/QCVOC.Api/Security/AccountQueryParameters.cs
@@ -1,9 +1,22 @@
-﻿using QCVOC.Api.Common;
+﻿// <copyright file="AccountQueryParameters.cs" company="JP Dillingham, Nick Acosta, et. al.">
+//     Copyright (c) JP Dillingham, Nick Acosta, et. al.. All rights reserved. Licensed under the GPLv3 license. See LICENSE file
+//     in the project root for full license information.
+// </copyright>
 
 namespace QCVOC.Api.Security
 {
+    using System.Diagnostics.CodeAnalysis;
+    using QCVOC.Api.Common;
+
+    /// <summary>
+    ///     Account request pagination and filtering options.
+    /// </summary>
+    [SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1623:PropertySummaryDocumentationMustMatchAccessors", Justification = "Omitted for Swashbuckle compatibility.")]
     public class AccountQueryParameters : QueryParameters
     {
+        /// <summary>
+        ///     The Role by which to filter results.
+        /// </summary>
         public Role? Role { get; set; }
     }
 }
