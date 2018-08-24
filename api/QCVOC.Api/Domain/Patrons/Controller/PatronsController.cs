37a4c4536523dda5c5b2ea3f4c56171f3063f2d4
JP Dillingham <jpdillingham@gmail.com>
2018-08-24 07:49:29 -0500

    begin cleanup and documentation of PatronsController

--- api/QCVOC.Api/Domain/Patrons/Controller/PatronsController.cs
+++ api/QCVOC.Api/Domain/Patrons/Controller/PatronsController.cs
@@ -1,4 +1,4 @@
-// <copyright file="AccountsController.cs" company="JP Dillingham, Nick Acosta, et. al.">
+// <copyright file="PatronsController.cs" company="JP Dillingham, Nick Acosta, et. al.">
 //     Copyright (c) JP Dillingham, Nick Acosta, et. al.. All rights reserved. Licensed under the GPLv3 license. See LICENSE file
 //     in the project root for full license information.
 // </copyright>
@@ -12,23 +12,74 @@ namespace QCVOC.Api.Domain.Patrons.Controller
     using Microsoft.AspNetCore.Mvc;
     using Microsoft.AspNetCore.Mvc.ModelBinding;
     using QCVOC.Api.Common.Data.Repository;
-    using QCVOC.Api.Domain.Patrons.Data.DTO;
     using QCVOC.Api.Domain.Patrons.Data.Model;
     using QCVOC.Api.Security;
 
-    [Authorize(Roles = nameof(Role.Administrator) + "," + nameof(Role.Supervisor))]
+    /// <summary>
+    ///     Provides endpoints for manipulation of Patron records.
+    /// </summary>
     [ApiVersion("1")]
     [Route("api/v{version:apiVersion}/[controller]")]
     [Produces("application/json")]
     [Consumes("application/json")]
     public class PatronsController : Controller
     {
+        /// <summary>
+        ///     Initializes a new instance of the <see cref="PatronsController"/> class.
+        /// </summary>
+        /// <param name="patronRepository">The repository used for Patron data access.</param>
         public PatronsController(IRepository<Patron> patronRepository)
         {
             PatronRepository = patronRepository;
         }
 
         private IRepository<Patron> PatronRepository { get; set; }
+
+        /// <summary>
+        ///     Returns a list of Patrons.
+        /// </summary>
+        /// <param name="filters">Optional filtering and pagination options.</param>
+        /// <returns>See attributes.</returns>
+        /// <response code="200">The list was retrieved successfully.</response>
+        /// <response code="401">Unauthorized.</response>
+        /// <response code="500">The server encountered an error while processing the request.</response>
+        [HttpGet("")]
+        [Authorize]
+        [ProducesResponseType(typeof(IEnumerable<Patron>), 200)]
+        [ProducesResponseType(401)]
+        [ProducesResponseType(typeof(Exception), 500)]
+        public IActionResult GetAll([FromQuery]PatronFilters filters)
+        {
+            return Ok(PatronRepository.GetAll(filters));
+        }
+
+        /// <summary>
+        ///     Returns the Patron matching the specified <paramref name="id"/>.
+        /// </summary>
+        /// <param name="id">The id of the Patron to retrieve.</param>
+        /// <returns>See attributes.</returns>
+        /// <response code="200">The Patron was retrieved successfully.</response>
+        /// <response code="400">The specified id was invalid.</response>
+        /// <response code="401">Unauthorized.</response>
+        /// <response code="404">A Patron matching the specified id could not be found.</response>
+        /// <response code="500">The server encountered an error while processing the request.</response>
+        [HttpGet("{id}")]
+        [Authorize]
+        [ProducesResponseType(typeof(IEnumerable<Patron>), 200)]
+        [ProducesResponseType(401)]
+        [ProducesResponseType(404)]
+        [ProducesResponseType(typeof(Exception), 500)]
+        public IActionResult Get(Guid id)
+        {
+            var patron = PatronRepository.Get(id);
+
+            if (patron == default(Patron))
+            {
+                return NotFound();
+            }
+
+            return Ok(patron);
+        }
 
         [HttpDelete("{id}")]
         [ProducesResponseType(typeof(OkResult), 200)]
@@ -55,48 +106,6 @@ namespace QCVOC.Api.Domain.Patrons.Controller
             }
 
             return Ok();
-        }
-
-        [HttpGet("{id}")]
-        [ProducesResponseType(typeof(PatronResponse), 200)]
-        [ProducesResponseType(404)]
-        [ProducesResponseType(typeof(ModelStateDictionary), 400)]
-        [ProducesResponseType(typeof(Exception), 500)]
-        public IActionResult Get(string id)
-        {
-            if (!Guid.TryParse(id, out Guid guid))
-            {
-                var err = new ModelStateDictionary();
-                err.AddModelError("id", "The requested Id must be a valid Guid.");
-
-                return BadRequest(err);
-            }
-
-            Patron patron = default(Patron);
-
-            try
-            {
-                patron = PatronRepository.Get(guid);
-            }
-            catch (Exception ex)
-            {
-                return StatusCode(500, new Exception("Error retrieving the specified Patron. See inner exception for details.", ex));
-            }
-
-            if (patron == default(Patron))
-            {
-                return NotFound();
-            }
-
-            return Ok(MapPatronResponseFrom(patron));
-        }
-
-        [HttpGet("")]
-        [ProducesResponseType(typeof(IEnumerable<PatronResponse>), 200)]
-        [ProducesResponseType(typeof(Exception), 500)]
-        public IActionResult GetAll()
-        {
-            return Ok(PatronRepository.GetAll().Select(a => MapPatronResponseFrom(a)));
         }
 
         [HttpPost("{id}")]
