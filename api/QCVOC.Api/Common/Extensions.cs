110947324c2e4350188f3d4dae914d0e277c5b12
JP Dillingham <jpdillingham@gmail.com>
2018-08-25 16:15:41 -0500

    add ClaimsPrincipal.GetId() extension method

---
+++ api/QCVOC.Api/Common/Extensions.cs
@@ -0,0 +1,36 @@
+﻿namespace QCVOC.Api.Common
+{
+    using System;
+    using System.Linq;
+    using System.Security.Claims;
+
+    /// <summary>
+    ///     Miscellaneous extension methods.
+    /// </summary>
+    public static class Extensions
+    {
+        /// <summary>
+        ///     Returns the value of the <see cref="ClaimTypes.NameIdentifier"/> claim for the specified <paramref name="user"/>
+        /// </summary>
+        /// <param name="user">The ClaimsPrincipal instance from which to retrieve the id.</param>
+        /// <returns>The retrieved id.</returns>
+        /// <exception cref="ArgumentNullException">Thrown when the supplied <see cref="ClaimsPrincipal"/> doesn't contain a <see cref="ClaimTypes.NameIdentifier"/> claim.</exception>
+        /// <exception cref="FormatException">Thrown when the <see cref="ClaimTypes.NameIdentifier"/> is not a valid Guid.</exception>
+        public static Guid GetId(this ClaimsPrincipal user)
+        {
+            var claim = user.Claims.Where(c => c.Type == ClaimTypes.NameIdentifier).FirstOrDefault();
+
+            if (claim == default(Claim))
+            {
+                throw new ArgumentNullException("The provided ClaimsPrincipal does not contain a NameIdentifier claim.");
+            }
+
+            if (Guid.TryParse(claim.Value, out var id))
+            {
+                return id;
+            }
+
+            throw new FormatException("The value of the NameIdentifier claim is not a valid Guid.");
+        }
+    }
+}
