cd2ba6aea7b1c365422c8904c809a8316b86a0f7
JP Dillingham <jpdillingham@gmail.com>
2018-08-25 18:52:11 -0500

    remove unused import

--- web/src/accounts/AccountDialog.js
+++ web/src/accounts/AccountDialog.js
@@ -1,8 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
-import { getGuid } from '../util';
-
 import { withStyles } from '@material-ui/core/styles';
 import { 
     Dialog,
