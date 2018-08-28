47fe0b8705a49d0dc3b305a7a9edc3848bf2e400
JP Dillingham <jpdillingham@gmail.com>
2018-08-27 22:17:22 -0500

    stub Patrons component

--- web/src/patrons/Patrons.js
+++ web/src/patrons/Patrons.js
@@ -1,22 +1,108 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import api from '../api';
 
 import { withStyles } from '@material-ui/core/styles';
 
+import ContentWrapper from '../shared/ContentWrapper';
+import Snackbar from '@material-ui/core/Snackbar';
+import { Card, CardContent, Typography, CircularProgress } from '@material-ui/core';
+
 const styles = {
-    root: {
-        flexGrow: 1,
+    fab: {
+        margin: 0,
+        top: 'auto',
+        right: 20,
+        bottom: 20,
+        left: 'auto',
+        position: 'fixed',
+        zIndex: 1000
+    },
+    card: {
+        minHeight: 125,
+    },
+    refreshSpinner: {
+        position: 'fixed',
+        left: 0,
+        right: 0,
+        marginLeft: 'auto',
+        marginRight: 'auto',
+        marginTop: 25,
     },
 };
 
 class Patrons extends Component {
-    render() {
-        let classes = this.props.classes;
+    state = {
+        patrons: [],
+        loadApi: {
+            isExecuting: false,
+            isErrored: false,
+        },
+        refreshApi: {
+            isExecuting: false,
+            isErrored: false,
+        },
+        snackbar: {
+            message: '',
+            open: false,
+        },
+    }
 
+    componentWillMount = () => {
+        this.refresh('loadApi');
+    }
+
+    refresh = (apiType) => {
+        this.setState({ [apiType]: { ...this.state[apiType], isExecuting: true }}, () => {
+            api.get('/v1/patrons')
+            .then(response => {
+                this.setState({ 
+                    patrons: response.data,
+                    [apiType]: { isExecuting: false, isErrored: false },
+                });
+            }, error => {
+                console.log(JSON.stringify(error.response.data.Message))
+                this.setState({ 
+                    [apiType]: { isExecuting: false, isErrored: true },
+                    snackbar: { message: error.response.data.Message, open: true },
+                }, () => console.log(this.state));
+            });
+        })
+    }
+
+    handleSnackbarClose = () => {
+        this.setState({ snackbar: { open: false }});
+    }
+
+    render() {
+        let { classes } = this.props;
+        let { loadApi, refreshApi, snackbar } = this.state;
+
+        console.log(snackbar)
         return (
-            <div className={classes.root}>
-                Patrons
+            <div>
+                <ContentWrapper api={loadApi}>
+                    <Card>
+                        <CardContent className={classes.card}>
+                            <Typography gutterBottom variant="headline" componet="h2">
+                                Patrons
+                            </Typography>
+                            {refreshApi.isExecuting ? 
+                                <CircularProgress size={30} color={'secondary'} className={classes.refreshSpinner}/> :
+                                <p>Patron list goes here</p>
+                            }
+                        </CardContent>
+                    </Card>
+                </ContentWrapper>
+                <Snackbar
+                    anchorOrigin={{ vertical: 'bottom', horizontal: 'center'}}
+                    open={snackbar.open}
+                    onClose={this.handleSnackbarClose}
+                    autoHideDuration={3000}
+                    message={<span id="message-id">{snackbar.message}</span>}
+                />
             </div>
+
         );
     }
 }
