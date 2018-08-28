843db04253656cd931cb8bcbc03577101cd65776
JP Dillingham <jpdillingham@gmail.com>
2018-08-28 07:41:34 -0500

    stub PatronList

---
+++ web/src/patrons/PatronList.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { sortByProp } from '../util';
+
+import { List, ListItem, ListItemIcon, ListItemText, ListItemSecondaryAction, IconButton } from '@material-ui/core';
+import { Person, Star, SupervisorAccount, LockOpen } from '@material-ui/icons'
+
+const getUserIcon = (role) => {
+    switch (role) {
+        case 'Administrator':
+            return <Star/>;
+        case 'Supervisor':
+            return <SupervisorAccount/>;
+        default:
+            return <Person/>;
+    }
+}
+
+const PatronList = (props) => {
+    let { accounts, onItemClick, onItemResetClick } = props;
+
+    return (
+        <List>
+            {accounts.sort(sortByProp('name')).map(a => 
+                <ListItem 
+                    key={a.id}
+                    button
+                    onClick={() => onItemClick(a)}
+                >
+                    <ListItemIcon>
+                        {getUserIcon(a.role)}
+                    </ListItemIcon>
+                    <ListItemText
+                        primary={a.name}
+                        secondary={a.role}
+                    />
+                    <ListItemSecondaryAction>
+                        <IconButton onClick={() => onItemResetClick(a)}>
+                            <LockOpen/>
+                        </IconButton>
+                    </ListItemSecondaryAction>
+                </ListItem>
+            )}
+        </List>
+    );
+}
+
+PatronList.propTypes = {
+    accounts: PropTypes.array.isRequired,
+    onItemClick: PropTypes.func.isRequired,
+};
+
+export default PatronList; 
