0c1d1eed7a20afccfbabc730b711c866238cdfd0
Will Burklund <waburklund@gmail.com>
2018-08-27 18:16:24 -0700

    Add copyright notice to web/src/accounts/*

--- web/src/accounts/AccountList.js
+++ web/src/accounts/AccountList.js
@@ -1,3 +1,8 @@
+/*
+    Copyright (c) QC Coders (JP Dillingham, Nick Acosta, et. al.). All rights reserved. Licensed under the GPLv3 license. See LICENSE file
+    in the project root for full license information.
+*/
+
 import React from 'react';
 import PropTypes from 'prop-types';
 import { sortByProp } from '../util';
