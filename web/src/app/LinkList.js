c5c766f5e38f024f5480a05ced3e60864de85aa8
JP Dillingham <jpdillingham@gmail.com>
2018-09-01 10:34:48 -0500

    add LinkList component

---
+++ web/src/app/LinkList.js
@@ -0,0 +1,18 @@
+/*
+    Copyright (c) QC Coders (JP Dillingham, Nick Acosta, Will Burklund, et. al.). All rights reserved. Licensed under the GPLv3 license. See LICENSE file
+    in the project root for full license information.
+*/
+
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const LinkList = (props) => {
+    return props.children
+        .map((child, index) => React.cloneElement(child, { key: index, onClick: props.onLinkClick }));
+}
+
+LinkList.propTypes = {
+  onLinkClick: PropTypes.func.isRequired,
+};
+
+export default LinkList; 
