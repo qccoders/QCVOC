c3bc4501e269880189ab20f25dfef5558ad35e75
JP Dillingham <jpdillingham@gmail.com>
2018-09-02 21:39:05 -0500

    add FilterType

---
+++ api/QCVOC.Api/Common/FilterType.cs
@@ -0,0 +1,43 @@
+﻿// <copyright file="FilterType.cs" company="QC Coders (JP Dillingham, Nick Acosta, et. al.)">
+//     Copyright (c) QC Coders (JP Dillingham, Nick Acosta, et. al.). All rights reserved. Licensed under the GPLv3 license. See LICENSE file
+//     in the project root for full license information.
+// </copyright>
+
+namespace QCVOC.Api.Common
+{
+    /// <summary>
+    ///     Determines the type of filter to be applied.
+    /// </summary>
+    public class FilterType
+    {
+        private FilterType(string value)
+        {
+            Value = value;
+        }
+
+        /// <summary>
+        ///     Gets a basic equality filter.
+        /// </summary>
+        public static new FilterType Equals => new FilterType("=");
+
+        /// <summary>
+        ///     Gets an IN filter.
+        /// </summary>
+        public static FilterType In => new FilterType("IN");
+
+        /// <summary>
+        ///     Gets a LIKE filter.
+        /// </summary>
+        public static FilterType Like => new FilterType("LIKE");
+
+        /// <summary>
+        ///     Gets a BETWEEN filter.
+        /// </summary>
+        public static FilterType Between => new FilterType("BETWEEN");
+
+        /// <summary>
+        ///     Gets the value of the filter.
+        /// </summary>
+        public string Value { get; }
+    }
+}
