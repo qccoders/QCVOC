a7db49a68859373aff94c47c624710e019ca5623
JP Dillingham <jpdillingham@gmail.com>
2018-09-05 08:29:41 -0500

    add CreationBy/Id, update header, sort

--- api/QCVOC.Api/Security/Data/Model/Account.cs
+++ api/QCVOC.Api/Security/Data/Model/Account.cs
@@ -1,5 +1,5 @@
-﻿// <copyright file="Account.cs" company="JP Dillingham, Nick Acosta, et. al.">
-//     Copyright (c) JP Dillingham, Nick Acosta, et. al.. All rights reserved. Licensed under the GPLv3 license. See LICENSE file
+﻿// <copyright file="Account.cs" company="QC Coders (JP Dillingham, Nick Acosta, et. al.)">
+//     Copyright (c) QC Coders (JP Dillingham, Nick Acosta, et. al.). All rights reserved. Licensed under the GPLv3 license. See LICENSE file
 //     in the project root for full license information.
 // </copyright>
 
@@ -14,9 +14,39 @@ namespace QCVOC.Api.Security.Data.Model
     public class Account : IEquatable<Account>
     {
         /// <summary>
+        ///     Gets the name of the user which created the Account.
+        /// </summary>
+        public string CreationBy { get; }
+
+        /// <summary>
+        ///     Gets or sets the id of the user which created the Account.
+        /// </summary>
+        public Guid CreationById { get; set; }
+
+        /// <summary>
+        ///     Gets or sets the Account creation date.
+        /// </summary>
+        public DateTime CreationDate { get; set; }
+
+        /// <summary>
         ///     Gets or sets the Account id.
         /// </summary>
         public Guid Id { get; set; }
+
+        /// <summary>
+        ///     Gets the name of the user which performed the last update.
+        /// </summary>
+        public string LastUpdateBy { get; }
+
+        /// <summary>
+        ///     Gets or sets the id of the user which performed the last update.
+        /// </summary>
+        public Guid LastUpdateById { get; set; }
+
+        /// <summary>
+        ///     Gets or sets the date on which the Account was last updated.
+        /// </summary>
+        public DateTime LastUpdateDate { get; set; }
 
         /// <summary>
         ///     Gets or sets the Account name.
@@ -39,26 +69,6 @@ namespace QCVOC.Api.Security.Data.Model
         public Role Role { get; set; }
 
         /// <summary>
-        ///     Gets or sets the Account creation date.
-        /// </summary>
-        public DateTime CreationDate { get; set; }
-
-        /// <summary>
-        ///     Gets or sets the date on which the Account was last updated.
-        /// </summary>
-        public DateTime LastUpdateDate { get; set; }
-
-        /// <summary>
-        ///     Gets the name of the user which performed the last update.
-        /// </summary>
-        public string LastUpdateBy { get; }
-
-        /// <summary>
-        ///     Gets or sets the id of the user which performed the last update.
-        /// </summary>
-        public Guid LastUpdateById { get; set; }
-
-        /// <summary>
         ///     Compares two Account instances.
         /// </summary>
         /// <param name="account">The Account to which to compare.</param>
@@ -76,8 +86,11 @@ namespace QCVOC.Api.Security.Data.Model
             && this.PasswordResetRequired == account.PasswordResetRequired
             && this.Role == account.Role
             && this.CreationDate == account.CreationDate
+            && this.CreationById == account.CreationById
+            && this.CreationBy == account.CreationBy
             && this.LastUpdateDate == account.LastUpdateDate
-            && this.LastUpdateById == account.LastUpdateById;
+            && this.LastUpdateById == account.LastUpdateById
+            && this.LastUpdateBy == account.LastUpdateBy;
         }
     }
 }
