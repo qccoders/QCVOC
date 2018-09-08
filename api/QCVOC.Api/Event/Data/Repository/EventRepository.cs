6728c4908b5907bf9fe9d204a8791586f7b1b58f
JP Dillingham <jpdillingham@gmail.com>
2018-09-08 17:46:55 -0500

    stub EventRepository

---
+++ api/QCVOC.Api/Event/Data/Repository/EventRepository.cs
@@ -0,0 +1,30 @@
+﻿// <copyright file="EventRepository.cs" company="QC Coders (JP Dillingham, Nick Acosta, et. al.)">
+//     Copyright (c) QC Coders (JP Dillingham, Nick Acosta, et. al.). All rights reserved. Licensed under the GPLv3 license. See LICENSE file
+//     in the project root for full license information.
+// </copyright>
+
+namespace QCVOC.Api.Event.Data.Repository
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using Dapper;
+    using QCVOC.Api.Common;
+    using QCVOC.Api.Common.Data.ConnectionFactory;
+    using QCVOC.Api.Common.Data.Repository;
+    using QCVOC.Api.Event.Data.Model;
+    using QCVOC.Api.Event;
+
+    /// <summary>
+    ///     Provides data access for <see cref="Event"/>.
+    /// </summary>
+    public class EventRepository : IRepository<Event>
+    {
+        public EventRepository(IDbConnectionFactory connectionFactory)
+        {
+            connectionFactory = connectionFactory;
+        }
+
+        private IDbConnectionFactory ConnectionFactory { get; }
+    }
+}
