430febc19e0cc09c8ed6e4692b48fa320edd7ddf
Will Burklund <waburklund@gmail.com>
2018-09-11 23:08:45 -0500

    Initial implementation of Services UI

---
+++ web/src/services/ServiceList.js
@@ -0,0 +1,42 @@
+/*
+    Copyright (c) QC Coders (JP Dillingham, Nick Acosta, Will Burklund, et. al.). All rights reserved. Licensed under the GPLv3 license. See LICENSE file
+    in the project root for full license information.
+*/
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import { List, ListItem, ListItemIcon, ListItemText } from '@material-ui/core';
+import { Shop } from '@material-ui/icons'
+
+import { sortByProp } from '../util'
+
+const ServiceList = (props) => {
+    let { services, onItemClick } = props;
+
+    return (
+        <List>
+            {services.sort(sortByProp('name')).map(s =>
+                <ListItem
+                    key={s.id}
+                    button
+                    onClick={() => onItemClick(s)}
+                >
+                    <ListItemIcon>
+                        <Shop/>
+                    </ListItemIcon>
+                    <ListItemText
+                        primary={s.name}
+                        secondary={s.description}
+                    />
+                </ListItem>
+            )}
+        </List>
+    );
+}
+
+ServiceList.propTypes = {
+    services: PropTypes.array.isRequired,
+    onItemClick: PropTypes.func.isRequired,
+};
+
+export default ServiceList;
--- web/src/services/Services.js
+++ web/src/services/Services.js
@@ -4,22 +4,94 @@
 */
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import api from '../api';
 
 import { withStyles } from '@material-ui/core/styles';
 
+import ContentWrapper from '../shared/ContentWrapper';
+import { Card, CardContent, Typography, CircularProgress } from '@material-ui/core';
+import ServiceList from './ServiceList';
+
 const styles = {
-    root: {
-        flexGrow: 1,
+    card: {
+        minHeight: 220,
+        maxWidth: 800,
+        margin: 'auto',
+    },
+    refreshSpinner: {
+        position: 'fixed',
+        left: 0,
+        right: 0,
+        marginLeft: 'auto',
+        marginRight: 'auto',
+        marginTop: 83,
     },
 };
 
 class Services extends Component {
+    state = {
+        services: [],
+        loadApi: {
+            isExecuting: false,
+            isErrored: false,
+        },
+        refreshApi: {
+            isExecuting: false,
+            isErrored: false,
+        },
+        snackbar: {
+            message: '',
+            open: false,
+        },
+    }
+
+    componentWillMount = () => {
+        this.refresh('refreshApi');
+    }
+
+    handleEditClick = (service) => {
+
+    }
+
+    refresh = (apiType) => {
+        this.setState({ [apiType]: { ...this.state[apiType], isExecuting: true}}, () => {
+            api.get('/v1/services?offset=0&limit=5000&orderBy=ASC')
+            .then(response => {
+                this.setState({
+                    services: response.data,
+                    [apiType]: { isExecuting: false, isErrored: false },
+                });
+            }, error => {
+                this.setState({
+                    [apiType]: { isExecuting: false, isErrored: true },
+                    snackbar: { message: error.response.data.Message, open: true },
+                });
+            });
+        })
+    }
+
     render() {
-        let classes = this.props.classes;
+        let { classes } = this.props;
+        let { services, loadApi, refreshApi, snackbar } = this.state;
 
         return (
-            <div className={classes.root}>
-                Services
+            <div>
+                <ContentWrapper api={loadApi}>
+                    <Card className={classes.card}>
+                        <CardContent>
+                            <Typography gutterBottom variant="headline" component="h2">
+                                Services
+                            </Typography>
+                            {refreshApi.isExecuting ?
+                                <CircularProgress size={30} color={'secondary'} className={classes.refreshSpinner}/> :
+                                <ServiceList
+                                    services={services}
+                                    onItemClick={this.handleEditClick}
+                                />
+                            }
+                        </CardContent>
+                    </Card>
+                </ContentWrapper>
             </div>
         );
     }
