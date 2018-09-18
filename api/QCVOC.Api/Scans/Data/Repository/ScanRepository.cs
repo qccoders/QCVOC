9a7b25cd39e57c981413bdf0d46d70f49fc8121c
JP Dillingham <jpdillingham@gmail.com>
2018-09-17 21:17:54 -0500

    implement Create()

--- api/QCVOC.Api/Scans/Data/Repository/ScanRepository.cs
+++ api/QCVOC.Api/Scans/Data/Repository/ScanRepository.cs
@@ -7,23 +7,56 @@ namespace QCVOC.Api.Scans.Data.Repository
 {
     using System;
     using System.Collections.Generic;
+    using Dapper;
     using QCVOC.Api.Common;
+    using QCVOC.Api.Common.Data.ConnectionFactory;
     using QCVOC.Api.Common.Data.Repository;
     using QCVOC.Api.Scans.Data.Model;
 
     /// <summary>
     ///     Provides data access for <see cref="Scan"/>.
     /// </summary>
-    public class ScanRepository : IRepository<Scan>
+    public class ScanRepository : ITripleKeyRepository<Scan>
     {
-        public Scan Create(Scan resource)
+        /// <summary>
+        ///     Initializes a new instance of the <see cref="ScanRepository"/> class.
+        /// </summary>
+        /// <param name="connectionFactory">The database connection factory used for data access.</param>
+        public ScanRepository(IDbConnectionFactory connectionFactory)
         {
-            throw new NotImplementedException();
+            ConnectionFactory = connectionFactory;
         }
 
-        public void Delete(Guid id)
+        private IDbConnectionFactory ConnectionFactory { get; }
+
+        public Scan Create(Scan scan)
         {
-            throw new NotImplementedException();
+            var builder = new SqlBuilder();
+
+            var query = builder.AddTemplate(@"
+                INSERT INTO scans
+                    (eventid, veteranid, serviceid, plusone, scandate, scanbyid, deleted)
+                VALUES
+                    (@eventid, @veteranid, @serviceid, @plusone, @scandate, @scanbyid, @deleted)
+            ");
+
+            builder.AddParameters(new
+            {
+                eventid = scan.EventId,
+                veteranid = scan.VeteranId,
+                serviceid = scan.ServiceId,
+                plusone = scan.PlusOne,
+                scandate = scan.ScanDate,
+                scanbyid = scan.ScanById,
+                deleted = false,
+            });
+
+            using (var db = ConnectionFactory.CreateConnection())
+            {
+                db.Execute(query.RawSql, query.Parameters);
+            }
+
+            return Get(scan.EventId, scan.VeteranId, scan.ServiceId);
         }
 
         public void Delete(Scan resource)
@@ -31,7 +64,12 @@ namespace QCVOC.Api.Scans.Data.Repository
             throw new NotImplementedException();
         }
 
-        public Scan Get(Guid id)
+        public void Delete(Guid eventId, Guid veteranId, Guid? serviceId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Scan Get(Guid eventId, Guid veteranId, Guid? serviceId)
         {
             throw new NotImplementedException();
         }
