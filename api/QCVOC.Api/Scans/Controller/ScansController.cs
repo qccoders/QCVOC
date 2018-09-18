5f99da5271646eac3235c3324792aedfd793729b
JP Dillingham <jpdillingham@gmail.com>
2018-09-18 08:17:54 -0500

    stub ScansController

---
+++ api/QCVOC.Api/Scans/Controller/ScansController.cs
@@ -0,0 +1,32 @@
+﻿// <copyright file="ScansController.cs" company="QC Coders (JP Dillingham, Nick Acosta, et. al.)">
+//     Copyright (c) QC Coders (JP Dillingham, Nick Acosta, et. al.). All rights reserved. Licensed under the GPLv3 license. See LICENSE file
+//     in the project root for full license information.
+// </copyright>
+
+namespace QCVOC.Api.Scans.Controller
+{
+    using Microsoft.AspNetCore.Mvc;
+    using QCVOC.Api.Common.Data.Repository;
+    using QCVOC.Api.Scans.Data.Model;
+
+    /// <summary>
+    ///     Provides endpoints for manipulation of Scan records.
+    /// </summary>
+    [ApiVersion("1")]
+    [Route("api/v{version:apiVersion}/[controller]")]
+    [Produces("application/json")]
+    [Consumes("application/json")]
+    public class ScansController : Controller
+    {
+        /// <summary>
+        ///     Initializes a new instance of the <see cref="ScansController"/> class.
+        /// </summary>
+        /// <param name="scanRepository">The repository used for Scan data access.</param>
+        public ScansController(ITripleKeyRepository<Scan> scanRepository)
+        {
+            ScanRepository = scanRepository;
+        }
+
+        private ITripleKeyRepository<Scan> ScanRepository { get; set; }
+    }
+}
