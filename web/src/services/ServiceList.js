665542a0babea0e84c9c3912a213812b0f8a8c35
JP Dillingham <jpdillingham@gmail.com>
2018-09-23 08:05:28 -0500

    split user and system defined services

--- web/src/services/ServiceList.js
+++ web/src/services/ServiceList.js
@@ -5,15 +5,34 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import { List, ListItem, ListItemIcon, ListItemText } from '@material-ui/core';
+import { List, ListItem, ListItemIcon, ListItemText, ListSubheader } from '@material-ui/core';
 import { Shop } from '@material-ui/icons'
 
 const ServiceList = (props) => {
     let { services, onItemClick } = props;
+    let userDefined = services.filter(s => s.id !== '00000000-0000-0000-0000-000000000000');
+    let systemDefined = services.filter(s => s.id === '00000000-0000-0000-0000-000000000000');
 
     return (
         <List>
-            {services.map(s =>
+            <ListSubheader>User Defined</ListSubheader>
+            {userDefined.map(s =>
+                <ListItem
+                    key={s.id}
+                    button
+                    onClick={() => onItemClick(s)}
+                >
+                    <ListItemIcon>
+                        <Shop/>
+                    </ListItemIcon>
+                    <ListItemText
+                        primary={s.name}
+                        secondary={s.description}
+                    />
+                </ListItem>
+            )}
+            <ListSubheader>System Defined</ListSubheader>
+            {systemDefined.map(s =>
                 <ListItem
                     key={s.id}
                     button
