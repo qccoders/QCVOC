36c3c388ce0461bede5e1d18cc8f1a2e72bba0f1
bkgramling <brennon.gramling@gmail.com>
2018-09-27 05:07:16 -0500

    src copyright update

--- web/src/index.js
+++ web/src/index.js
@@ -1,5 +1,5 @@
 /*
-    Copyright (c) QC Coders (JP Dillingham, Nick Acosta, Will Burklund, et. al.). All rights reserved. Licensed under the GPLv3 license. See LICENSE file
+    Copyright (c) QC Coders. All rights reserved. Licensed under the GPLv3 license. See LICENSE file
     in the project root for full license information.
 */
 import React from 'react';
