f4a9e42500107fdf43b37c91b995005cb617c839
Will Burklund <waburklund@gmail.com>
2018-10-02 13:41:16 -0500

    Show progress spinner during page load

--- mobile-new/app/src/main/java/org/qccoders/qcvoc/MainActivity.java
+++ mobile-new/app/src/main/java/org/qccoders/qcvoc/MainActivity.java
@@ -6,27 +6,47 @@
 package org.qccoders.qcvoc;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.webkit.JavascriptInterface;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
 
 import com.google.android.gms.samples.vision.barcodereader.BarcodeCaptureActivity;
 import com.google.android.gms.vision.barcode.Barcode;
 
 public class MainActivity extends AppCompatActivity {
     private WebView webview;
+    private ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        progressBar = findViewById(R.id.loading_spinner);
+        progressBar.setVisibility(View.GONE);
+
         webview = findViewById(R.id.webView);
+        webview.setWebViewClient(new WebViewClient() {
+            @Override
+            public void onPageStarted(WebView view, String url, Bitmap favicon) {
+                super.onPageStarted(view, url, favicon);
+                progressBar.setVisibility(View.VISIBLE);
+            }
+
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                super.onPageFinished(view, url);
+                progressBar.setVisibility(View.GONE);
+            }
+        });
+
         webview.addJavascriptInterface(this, "Android");
-        webview.setWebViewClient(new WebViewClient());
         webview.getSettings().setJavaScriptEnabled(true);
         webview.getSettings().setDomStorageEnabled(true);
         webview.loadUrl("http://qcvoc-dev.s3-website-us-east-1.amazonaws.com");
