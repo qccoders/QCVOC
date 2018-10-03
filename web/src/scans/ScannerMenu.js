c682964baa6f1aed633469df8193f7c388b5188a
JP Dillingham <jpdillingham@gmail.com>
2018-10-03 08:11:54 -0500

    remove unused stuff

--- web/src/scans/ScannerMenu.js
+++ web/src/scans/ScannerMenu.js
@@ -5,19 +5,15 @@
 
 import React, { Component } from 'react';
 import PropTypes from 'proptypes';
-import api from '../api';
 
 import IconButton from '@material-ui/core/IconButton';
-import Typography from '@material-ui/core/Typography';
-import { LockOpen, ExitToApp, AccountCircle }  from '@material-ui/icons';
+import { LockOpen, MoreVert }  from '@material-ui/icons';
 import ConfirmDialog from '../shared/ConfirmDialog';
-import { Badge, Menu, MenuItem, Snackbar, ListItemIcon, ListItemText } from '@material-ui/core';
-import PasswordResetDialog from '../security/PasswordResetDialog';
+import { Menu, MenuItem, ListItemIcon, ListItemText } from '@material-ui/core';
 
 const styles = {
     container: {
-        marginLeft: 'auto',
-        whiteSpace: 'pre',
+        display: 'inline',
     },
     caption: {
         marginRight: 5,
@@ -29,6 +25,10 @@ const styles = {
     menu: {
         marginTop: 40,
     },
+    iconMenu: {
+        float: 'right',
+        marginTop: -8,
+    },
 }
 
 const initialState = {
@@ -36,35 +36,16 @@ const initialState = {
         anchorEl: undefined,
         open: false,
     },
+    confirmDialog: {
+        open: false,
+    },
 };
 
 class ScannerMenu extends Component {
     state = initialState;
 
-    logout = () => {
-        return api.post('v1/security/logout')
-        .then(() => this.props.onLogout());
-    }
-
     handleMenuClick = (event) => {
         this.setState({ menu: { anchorEl: event.currentTarget, open: true }});
-    }
-
-    handleLogoutClick = () => {
-        this.setState({ 
-            menu: { open: false },
-            confirmDialog: { open: true }
-        });
-    }
-
-    handleResetPasswordClick = () => {
-        this.setState({ 
-            menu: { open: false },
-            passwordResetDialog: { 
-                open: true, 
-                account: { id: this.props.credentials.id, name: this.props.credentials.name }
-            }
-        });
     }
 
     handleMenuClose = () => {
@@ -75,37 +56,17 @@ class ScannerMenu extends Component {
         this.setState({ confirmDialog: { open: false }});
     }
 
-    handleSnackbarClose = () => {
-        this.setState({ snackbar: { open: false }});
-    }
-
-    handlePasswordResetDialogClose = (result) => {
-        this.setState({
-            passwordResetDialog: { open: false },
-        }, () => { 
-            if (result) { 
-                this.setState({ snackbar: { message: result, open: true }}, () => this.props.onPasswordReset());
-            }
-        });
-    }
-
     render() {
-        let { credentials } = this.props;
-        let { menu, passwordResetDialog } = this.state;
+        let { menu } = this.state;
 
         return (
             <div style={styles.container}>
-                <Typography color="inherit" style={styles.caption}>{credentials.name}</Typography>
                 <IconButton
                     color="inherit"
+                    style={styles.iconMenu}
                     onClick={this.handleMenuClick}
                 >
-                    {credentials.passwordResetRequired ? 
-                        <Badge badgeContent={'!'} color="secondary">
-                            <AccountCircle style={styles.icon}/>
-                        </Badge> :
-                        <AccountCircle style={styles.icon}/>
-                    }
+                    <MoreVert/>
                 </IconButton>
                 <Menu
                     open={menu.open}
@@ -115,48 +76,23 @@ class ScannerMenu extends Component {
                 >
                     <MenuItem onClick={this.handleResetPasswordClick}>
                         <ListItemIcon>
-                            {credentials.passwordResetRequired ? 
-                                <Badge badgeContent={'!'} color="secondary">
-                                    <LockOpen/>
-                                </Badge> :
-                                <LockOpen/>
-                            }
+                            <LockOpen/>
                         </ListItemIcon>
                         <ListItemText>
-                            Update Password
-                        </ListItemText>
-                    </MenuItem>
-                    <MenuItem onClick={this.handleLogoutClick}>
-                        <ListItemIcon>
-                            <ExitToApp/>
-                        </ListItemIcon>
-                        <ListItemText>
-                            Log Out
+                            Reset Scanner
                         </ListItemText>
                     </MenuItem>
                 </Menu>
                 <ConfirmDialog
-                    title={'Confirm Log Out'}
-                    prompt={'Log Out'}
+                    title={'Confirm Reset'}
+                    prompt={'Reset'}
                     open={this.state.confirmDialog.open}
-                    onConfirm={this.logout}
+                    onConfirm={this.props.onReset}
                     onClose={this.handleConfirmDialogClose}
                     suppressCloseOnConfirm
                 >
-                    <p>Are you sure you want to log out?</p>
+                    <p>Are you sure you want reset the Scanner?</p>
                 </ConfirmDialog>
-                <PasswordResetDialog
-                    open={passwordResetDialog.open}
-                    account={passwordResetDialog.account}
-                    onClose={this.handlePasswordResetDialogClose}
-                />
-                <Snackbar
-                    anchorOrigin={{ vertical: 'bottom', horizontal: 'center'}}
-                    open={this.state.snackbar.open}
-                    onClose={this.handleSnackbarClose}
-                    autoHideDuration={3000}
-                    message={<span id="message-id">{this.state.snackbar.message}</span>}
-                />
             </div>
         );
     }
