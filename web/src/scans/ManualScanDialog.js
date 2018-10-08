bd81167395a59ef0935f5f34ff7ec823f687b239
JP Dillingham <jpdillingham@gmail.com>
2018-10-07 22:00:03 -0500

    add input and validation

--- web/src/scans/ManualScanDialog.js
+++ web/src/scans/ManualScanDialog.js
@@ -12,6 +12,7 @@ import {
     DialogActions,
     Button,
     DialogContent,
+    TextField,
 } from '@material-ui/core';
 
 const styles = {
@@ -24,17 +25,65 @@ const styles = {
     },
 };
 
+const initialState = {
+    cardNumber: undefined,
+    validation: {
+        cardNumber: undefined,
+    },
+};
+
 class ManualScanDialog extends Component {
+    state = initialState;
+
+    componentWillReceiveProps = (nextProps) => {
+        if (nextProps.open && !this.props.open) {
+            this.setState({ 
+                ...initialState
+            });
+        }
+    }
+
     handleCancelClick = () => {
         this.props.onClose(undefined);
     }
 
     handleEnterClick = () => {
-        this.props.onClose(1111);
+        this.validate().then(result => {
+            if (result.isValid) {
+                this.props.onClose(this.state.cardNumber);
+            }
+        });
+    }
+
+    handleChange = (event) => {
+        this.setState({ 
+            cardNumber: event.target.value,
+            validation: { cardNumber: undefined },
+        });
+    }
+
+    validate = () => {
+        let result = { ...initialState.validation };
+
+        if (this.state.cardNumber === undefined) {
+            result.cardNumber = 'A Card Number is required.';
+        }
+
+        if (isNaN(this.state.cardNumber)) {
+            result.cardNumber = 'The Card Number must contain only numbers.';
+        }
+
+        return new Promise(resolve => {
+            this.setState({ validation: result }, () => {
+                result.isValid = JSON.stringify(result) === JSON.stringify(initialState.validation);
+                resolve(result);
+            });                
+        })
     }
     
     render() {
         let { classes, open } = this.props;
+        let { validation } = this.state;
         
         return (
             <Dialog 
@@ -45,6 +94,16 @@ class ManualScanDialog extends Component {
             >
                 <DialogTitle>Enter Card Number</DialogTitle>
                 <DialogContent>
+                    <TextField
+                        style={{marginTop: 0}}
+                        id="cardNumber"
+                        label="Card Number"
+                        type="numeric"
+                        error={validation.cardNumber !== undefined}
+                        helperText={validation.cardNumber}
+                        fullWidth
+                        onChange={(event) => this.handleChange(event)}
+                    />
                 </DialogContent>
                 <DialogActions>
                     <Button 
