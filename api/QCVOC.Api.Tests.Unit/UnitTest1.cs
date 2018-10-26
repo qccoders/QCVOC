4b7548cae4ebf1c7dd5f886062997c16f5a9d5c3
JP Dillingham <jpdillingham@gmail.com>
2018-10-25 21:42:15 -0500

    update copyright text, minor cleanup throughout

--- api/QCVOC.Api.Tests.Unit/UnitTest1.cs
+++ api/QCVOC.Api.Tests.Unit/UnitTest1.cs
@@ -1,14 +1,17 @@
-using System;
-using Xunit;
+// <copyright file="UnitTest1.cs" company="QC Coders (JP Dillingham, Nick Acosta, et. al.)">
+//     Copyright (c) QC Coders (JP Dillingham, Nick Acosta, et. al.). All rights reserved. Licensed under the GPLv3 license. See LICENSE file
+//     in the project root for full license information.
+// </copyright>
 
 namespace QCVOC.Api.Tests.Unit
 {
+    using Xunit;
+
     public class UnitTest1
     {
         [Fact]
         public void Test1()
         {
-
         }
     }
 }
