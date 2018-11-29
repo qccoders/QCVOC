da48b184f2d37b7a8b6cea0080b916dc9e0f4d37
JP Dillingham <jpdillingham@gmail.com>
2018-11-28 21:24:44 -0600

    add IVeteranRepository

---
+++ api/QCVOC.Api/Veterans/Data/Repository/IVeteranRepository.cs
@@ -0,0 +1,24 @@
+﻿// <copyright file="IVeteranRepository.cs" company="QC Coders">
+//     Copyright (c) QC Coders. All rights reserved. Licensed under the GPLv3 license. See LICENSE file
+//     in the project root for full license information.
+// </copyright>
+
+namespace QCVOC.Api.Veterans.Data.Repository
+{
+    using System;
+    using QCVOC.Api.Common.Data.Repository;
+    using QCVOC.Api.Veterans.Data.Model;
+
+    /// <summary>
+    ///     Provides data access for <see cref="Veteran"/>.
+    /// </summary>
+    public interface IVeteranRepository : ISingleKeyRepository<Veteran>
+    {
+        /// <summary>
+        ///     Retrieves the base 64 encoded photo for the Veteran matching the specified <paramref name="id"/>.
+        /// </summary>
+        /// <param name="id">The id of the <see cref="Veteran"/> to retrieve.</param>
+        /// <returns>The base 64 encoded photo for the Veteran matching the specified id.</returns>
+        string GetPhotoBase64(Guid id);
+    }
+}
