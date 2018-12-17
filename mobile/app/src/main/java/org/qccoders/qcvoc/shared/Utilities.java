97dce513d6dbdd498c0faa802089c36cbf4c2de4
Will Burklund <waburklund@gmail.com>
2018-12-17 17:15:45 -0600

    Add license headers

--- mobile/app/src/main/java/org/qccoders/qcvoc/shared/Utilities.java
+++ mobile/app/src/main/java/org/qccoders/qcvoc/shared/Utilities.java
@@ -1,3 +1,8 @@
+/*
+    Copyright (c) QC Coders. All rights reserved. Licensed under the GPLv3 license. See LICENSE file
+    in the project root for full license information.
+*/
+
 package org.qccoders.qcvoc.shared;
 
 import android.net.Uri;
