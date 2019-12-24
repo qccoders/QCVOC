a20d7c379077e581963f293f5b6dc4da374dc9c1
JP Dillingham <jpdillingham@gmail.com>
2019-12-23 20:13:08 -0600

    add endpoint for veteran dataset, docs

--- api/QCVOC.Api/Reports/Data/Repository/ReportRepository.cs
+++ api/QCVOC.Api/Reports/Data/Repository/ReportRepository.cs
@@ -9,9 +9,17 @@ namespace QCVOC.Api.Reports.Data
     using System.Collections.Generic;
     using Dapper;
     using QCVOC.Api.Common.Data.ConnectionFactory;
+    using QCVOC.Api.Veterans.Data.Model;
 
+    /// <summary>
+    ///     Provides data access for report datasets.
+    /// </summary>
     public class ReportRepository : IReportRepository
     {
+        /// <summary>
+        ///     Initializes a new instance of the <see cref="ReportRepository"/> class.
+        /// </summary>
+        /// <param name="connectionFactory">The database connection factory used for data access.</param>
         public ReportRepository(IDbConnectionFactory connectionFactory)
         {
             ConnectionFactory = connectionFactory;
@@ -19,6 +27,12 @@ namespace QCVOC.Api.Reports.Data
 
         private IDbConnectionFactory ConnectionFactory { get; }
 
+        /// <summary>
+        ///     Gets the event master report dataset.
+        /// </summary>
+        /// <param name="startTime">The start time.</param>
+        /// <param name="endTime">The end time.</param>
+        /// <returns>The event master dataset.</returns>
         public IEnumerable<EventMaster> GetEventMaster(DateTime startTime, DateTime endTime)
         {
             var builder = new SqlBuilder();
@@ -45,6 +59,41 @@ namespace QCVOC.Api.Reports.Data
             using (var db = ConnectionFactory.CreateConnection())
             {
                 return db.Query<EventMaster>(query.RawSql, query.Parameters);
+            }
+        }
+
+        /// <summary>
+        ///     Retrieves all Veterans, less `<see cref="Veteran.PhotoBase64"/>.
+        /// </summary>
+        /// <returns>A list of Veterans</returns>
+        public IEnumerable<Veteran> GetVeterans()
+        {
+            var builder = new SqlBuilder();
+
+            var query = builder.AddTemplate($@"
+                SELECT
+                    v.id,
+                    v.cardnumber,
+                    v.firstname,
+                    v.lastname,
+                    v.lastupdatedate,
+                    a.name AS lastupdateby,
+                    v.lastupdatebyid,
+                    v.address,
+                    v.primaryphone,
+                    v.email,
+                    v.enrollmentdate,
+                    v.enrollmentbyid,
+                    b.name AS enrollmentby,
+                    v.verificationmethod
+                FROM veterans v
+                LEFT JOIN accounts a ON v.lastupdatebyid = a.id
+                LEFT JOIN accounts b ON v.enrollmentbyid = b.id
+            ");
+
+            using (var db = ConnectionFactory.CreateConnection())
+            {
+                return db.Query<Veteran>(query.RawSql, query.Parameters);
             }
         }
     }
