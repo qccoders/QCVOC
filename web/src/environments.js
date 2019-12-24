cd09b8baf70040e39cc3734c1492a91982cd3bb9
JP Dillingham <jpdillingham@gmail.com>
2019-12-24 15:45:15 -0600

    fix url again

--- web/src/environments.js
+++ web/src/environments.js
@@ -5,7 +5,7 @@
 
 export const PROD = {
     name: 'prod',
-    hostname: 'qcvoc-prod.s3-website.us-east-1.amazonaws.com',
+    hostname: 'qcvoc-prod.s3-website-us-east-1.amazonaws.com',
     apiRoot: 'https://lv8t84qik7.execute-api.us-east-2.amazonaws.com/prod/api',
     color: 'primary',
 };
